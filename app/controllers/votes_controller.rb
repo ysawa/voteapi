47ad0dcc5d5253f19be70d75add62c00ac8d5ecc
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-10-04 01:03:51 +0900

    modify: allow remote hosts

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,6 +2,7 @@ class VotesController < ApplicationController
   respond_to :json, only: %i(count destroy index ranking)
   respond_to :html, only: %i(new)
   skip_before_filter :verify_authenticity_token, only: %i(create destroy)
+  before_action :allow_remote_hosts
   before_action :set_vote, only: %i(destroy)
   before_action :set_votes_by_category, only: %i(count index ranking)
 
