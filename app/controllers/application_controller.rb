4af3c26dee3a2662573aa61a388878f2013de374
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-10-04 01:13:02 +0900

    modify: allow remote hosts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,8 @@ class ApplicationController < ActionController::Base
 protected
   def allow_remote_hosts
     headers["Access-Control-Allow-Origin"] = "*"
+    headers['Access-Control-Allow-Headers'] = 'X-AUTH-TOKEN, X-API-VERSION, X-Requested-With, Content-Type, Accept, Origin'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
   end
 end
