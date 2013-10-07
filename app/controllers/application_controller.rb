e831236f4b8ceb0c4f382698c1d639c15b580043
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-10-07 23:23:15 +0900

    modify: cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter: allow_cors_requests
+  before_filter :allow_cors_requests
 
   def allow_cors
     headers["Access-Control-Allow-Origin"] = "*"
